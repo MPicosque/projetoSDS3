0a75ed4af1a536d4cd3c6419acaf6e7bb35965ea
Marcos Francisco Picosque <mpicosque@gmail.com>
2021-05-03 22:27:08 -0300

    BarChart with @Type error - react-apexcharts not in @type

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
@@ -7,7 +8,18 @@ function App() {
     <>
       <NavBar />
       <div className = "container">
-        <h1 className="text-primary">Olá mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5>Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5>Todas vendas</h5>
+            
+          </div>
+        </div>
 
         <DataTable />
       </div>
