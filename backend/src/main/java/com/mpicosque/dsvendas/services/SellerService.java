9f31059f13f937606d3a641a0638280b9efb96dc
Marcos Francisco Picosque <mpicosque@gmail.com>
2021-05-05 11:26:53 -0300

    Layers

---
+++ backend/src/main/java/com/mpicosque/dsvendas/services/SellerService.java
@@ -0,0 +1,23 @@
+package com.mpicosque.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mpicosque.dsvendas.dto.SellerDTO;
+import com.mpicosque.dsvendas.entities.Seller;
+import com.mpicosque.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
